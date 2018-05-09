f0bcb363fd1b2df6f61bf7515e4afa01bccf6213
cwjcyberfox <3607257+cwjcyberfox@users.noreply.github.com>
2018-05-09 08:04:32 +0000

    Create get_dayofweek2.cmd

---
+++ get_dayofweek2.cmd
@@ -0,0 +1,4 @@
+
+@echo offsetlocalfor /f "delims=" %%a in ('wmic path win32_localtime get dayofweek /format:list ') do for /f "delims=" %%d in ("%%a") do set %%d
+echo day of the week : %dayofweek%endlocal
+pause
